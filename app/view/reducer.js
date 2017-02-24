42d9ac89006f3bda8b1fa3aad2e2f8b9566b171a
Kate Leslie <thisizk8@gmail.com>
2017-02-23 23:14:12 -0600

    working on html & styles. copied some js from last assignment, but not sure if that's going to work.

---
+++ app/view/reducer.js
@@ -0,0 +1,12 @@
+
+//
+// export default function reducer(state, action) {
+//   switch (action.type) {
+//     case 'CONTACT@CREATE':
+//       return { [action.data, ...state.contact] };
+//     case 'CONTACT@FIND_ALL':
+//       return { contact: action.data };
+//     default:
+//       return state || { contact: [] };
+//     }
+// };
